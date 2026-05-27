1f193f21ee6ecc0c8e2f3fb99174813c9e9c918e
jarvis425 <zulqarnainshahalam@gmail.com>
2026-05-28 04:20:20 +0500

    agent: Act as an elite, fully autonomous Senior Flutter Architect a

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,8 @@ import 'package:master_agent_repo/screens/home_screen.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
